ab36764034ccb70be3eaa990e70fb9356afaca9c
Md Razi Ullah <mdraziullah631@gmail.com>
2024-03-29 21:50:43 +0530

    Add XPagination

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import XPagination from './XPagination'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <XPagination />
+      </div>
+    </>
+  )
+}
+
+export default App
